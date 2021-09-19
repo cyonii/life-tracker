3092c17a1414e8055a56cca269d6bcbfd81029e7
CY Kalu <cy23@tutanota.com>
2021-09-19 22:19:37 +0000

    Add react-toastify package and load it's assets

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import './assets/scss/style.scss';
 import reportWebVitals from './reportWebVitals';
@@ -7,6 +9,7 @@ import reportWebVitals from './reportWebVitals';
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToastContainer position="bottom-left" />
   </React.StrictMode>,
   document.getElementById('root'),
 );
