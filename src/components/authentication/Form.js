0993de39f907cebc56ff8de66b9cc05a8c40caad
CY Kalu <cy23@tutanota.com>
2021-09-20 12:42:18 +0000

    Authenticte with api wrapper and flash toasts

--- src/components/authentication/Form.js
+++ src/components/authentication/Form.js
@@ -1,37 +1,76 @@
-import React from 'react';
-import axios from 'axios';
+import { useState } from 'react';
 import { Form as BSForm, Button } from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import { authenticateUser } from '../../backend';
 
-export default function Form() {
-  const handleSubmit = (e) => {
+const Form = () => {
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    toast.dismiss();
+    setIsDisabled(true);
 
     const email = e.target.email.value;
     const password = e.target.password.value;
 
-    axios.post('http://localhost:3000/api/v1/auth', { email, password })
-      .then((res) => {
-        localStorage.setItem('authToken', res.data.auth_token);
-        localStorage.setItem('user', res.data.user);
-      })
-      .catch((err) => (err));
+    toast.promise(authenticateUser({ email, password }), {
+      pending: { render: () => 'Authenticating...' },
+      success: {
+        render: ({ data }) => {
+          localStorage.setItem('authToken', data.auth_token);
+          setTimeout(() => window.location.reload(), 1500);
+          return 'Authenticated!';
+        },
+      },
+      error: {
+        autoClose: false,
+        render: () => {
+          setIsDisabled(false);
+          return 'Authentication failed!';
+        },
+      },
+    });
   };
 
   return (
     <BSForm className="px-3 py-5 shadow border border-4 border-primary alert-primary rounded-2" onSubmit={handleSubmit}>
+      <hr className="bg-transparent" />
       <BSForm.Group className="mb-3" controlId="formBasicEmail">
         <BSForm.Label className="mb-0">Email address</BSForm.Label>
-        <BSForm.Control className="border-primary border-2" name="email" type="email" required placeholder="Enter email address" />
+        <BSForm.Control
+          className="border-primary border-2"
+          name="email"
+          type="email"
+          required
+          placeholder="Enter email address"
+          disabled={isDisabled}
+        />
       </BSForm.Group>
 
       <BSForm.Group className="mb-3" controlId="formBasicPassword">
         <BSForm.Label className="mb-0">Password</BSForm.Label>
-        <BSForm.Control className="border-primary border-2" name="password" type="password" required placeholder="Enter password" />
+        <BSForm.Control
+          className="border-primary border-2"
+          name="password"
+          type="password"
+          required
+          placeholder="Enter password"
+          disabled={isDisabled}
+        />
       </BSForm.Group>
 
-      <Button variant="primary" className="fw-bold text-white border border-2 px-5 border-white shadow" type="submit">
+      <Button
+        variant="primary"
+        className="fw-bold text-white border border-2 px-5 border-white shadow"
+        type="submit"
+        disabled={isDisabled}
+      >
         Login
       </Button>
+      <hr className="bg-transparent" />
     </BSForm>
   );
-}
+};
+
+export default Form;
