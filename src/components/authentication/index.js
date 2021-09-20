91061ca5aa475d0b90cea0f78a46460b015a5b35
CY Kalu <cy23@tutanota.com>
2021-09-20 14:11:59 +0000

    Remove row alignment

--- src/components/authentication/index.js
+++ src/components/authentication/index.js
@@ -12,7 +12,7 @@ export default function Authentication() {
   return (
     <Container className="py-5">
       <hr className="my-5 bg-transparent d-none d-md-block" />
-      <Row className="justify-content-center align-items-center gy-5 gy-md-0">
+      <Row className="justify-content-center gy-5 gy-md-0">
         <Col xs={12} md={6} lg={4} xl={3}>
           <p className="text-md-end text-muted mb-0">Welcome, human 👋</p>
           <h1 className="display-6 text-primary fw-bold">Life Tracker App</h1>
