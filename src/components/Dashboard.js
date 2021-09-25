97638d4aca324e4ea619bcb29b5606cc021791a8
CY Kalu <cy23@tutanota.com>
2021-09-25 18:20:40 +0000

    Add column definition

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,16 @@
-import { Tab } from 'react-bootstrap';
+import { Tab, Col } from 'react-bootstrap';
 import TabNav from './TabNav';
 import TabContent from './TabContent';
 
 export default function Dashboard() {
   return (
-    <div id="dashboard" className="d-flex flex-column vh-100">
-      <Tab.Container id="dashboard-tabs" defaultActiveKey="add">
-        <TabContent />
-        <TabNav />
-      </Tab.Container>
+    <div id="dashboard" className="vh-100 alert-dark">
+      <Col xs={12} sm={10} md={8} lg={6} xl={5} className="d-flex flex-column h-100 bg-white mx-auto">
+        <Tab.Container defaultActiveKey="add">
+          <TabContent />
+          <TabNav />
+        </Tab.Container>
+      </Col>
     </div>
   );
 }
