cd97cfd0b11c699a8e2bda4da5963f7e2c955c3c
CY Kalu <cy23@tutanota.com>
2021-09-30 09:42:52 +0000

    Refactor: remove WET code

--- src/components/TabNav.js
+++ src/components/TabNav.js
@@ -1,32 +1,23 @@
 import { Nav } from 'react-bootstrap';
 
-const TabNav = () => (
-  <Nav fill variant="pills" justify id="dashboard-nav" className="mt-auto">
+const TabNav = () => {
+  const navItem = (eventKey, text, iconClass) => (
     <Nav.Item>
-      <Nav.Link eventKey="add-record" className="h-100">
-        <i className="bi bi-plus-circle-fill fs-4 mb-1" />
-        Add record.
+      <Nav.Link eventKey={eventKey} className="h-100">
+        <i className={`bi ${iconClass} fs-4 mb-1`} />
+        {text}
       </Nav.Link>
     </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="records" className="h-100">
-        <i className="bi bi-graph-up fs-4 mb-1" />
-        Track.it
-      </Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="progress-report" className="h-100">
-        <i className="bi bi-pie-chart-fill fs-4 mb-1" />
-        Your progress
-      </Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link eventKey="more" className="h-100">
-        <i className="bi bi-three-dots fs-4 mb-1" />
-        More
-      </Nav.Link>
-    </Nav.Item>
-  </Nav>
-);
+  );
+
+  return (
+    <Nav fill variant="pills" justify id="dashboard-nav" className="mt-auto">
+      {navItem('add-record', 'Add record.', 'bi-plus-circle-fill')}
+      {navItem('records', 'Track.it', 'bi-graph-up')}
+      {navItem('progress-report', 'Your progress', 'bi-pie-chart-fill')}
+      {navItem('more', 'More', 'bi-three-dots')}
+    </Nav>
+  );
+};
 
 export default TabNav;
