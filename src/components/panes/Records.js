5b715de19ee4e858d682ad81143668c31e1e84ef
CY Kalu <cy23@tutanota.com>
2021-10-03 20:25:04 +0000

    Display user records

--- src/components/panes/Records.js
+++ src/components/panes/Records.js
@@ -1,3 +1,24 @@
-const Records = () => <div>Record List</div>;
+import { useEffect, useState } from 'react';
+import { ListGroup } from 'react-bootstrap';
+import RecordItem from '../RecordItem';
+import { getRecords } from '../../backend';
+
+const Records = () => {
+  const [records, setRecords] = useState([]);
+
+  useEffect(async () => {
+    await getRecords()
+      .then((res) => setRecords(res.data.data))
+      .catch((err) => err);
+  }, []);
+
+  return (
+    <div className="container">
+      <ListGroup variant="flush" className="p-0">
+        {records.map((record) => <RecordItem key={record.id} record={record} />)}
+      </ListGroup>
+    </div>
+  );
+};
 
 export default Records;
