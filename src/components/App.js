dffc18c285c6274f4f50a7dcea6d9225b10d78f6
CY Kalu <cy23@tutanota.com>
2021-10-03 08:47:26 +0000

    Refactor, keep code cleaner

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,13 @@ import Authentication from './authentication';
 import Dashboard from './Dashboard';
 
 function App() {
-  // display the authentication component if the user is not logged in
-  if (localStorage.getItem('authToken') === null) return <Authentication />;
+  const authToken = localStorage.getItem('authToken');
 
+  // display the authentication component if the user is not logged in
+  if (!authToken) {
+    localStorage.clear();
+    return <Authentication />;
+  }
   return <Dashboard />;
 }
 
